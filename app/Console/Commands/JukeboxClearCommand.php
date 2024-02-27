fd80d3b595466d241f70447f48192165a33bee90
Andres Rojas Moran <arojasmoran@gmail.com>
2024-02-27 18:37:15 +0000

    Commands: add `jukebox:clear` command, it will effectively empty the `jukebox` table

---
+++ app/Console/Commands/JukeboxClearCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Jukebox;
+use Illuminate\Console\Command;
+
+class JukeboxClearCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jukebox:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'clear queue and current playing track';
+
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle(): void
+    {
+        $queued = Jukebox::where('playing', false)->delete();
+        $playing = Jukebox::where('playing', true)->delete();
+
+        $this->info("The playlist has been cleared sucessfully!");
+    }
+}
